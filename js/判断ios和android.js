38c1bf6f14b759d897b0d7013d5667f0f015730a
zouxinxian <zouxinxian@smzdm.com>
2019-10-09 10:12:15 +0800

    判断终端

--- js/判断ios和android.js
+++ js/判断ios和android.js
@@ -1,3 +1,37 @@
+import jsonp from 'jsonp';
+import getClient from 'src/commons/utils/getClient.js';
+var baseUrl = 'https://zhiyou.smzdm.com/';
+
+/* eslint-disable */
+export default {
+    // 判断用户是否登录，此接口不支持跨域请求，所以用了jsonp，补上axios的不足。https://github.com/mzabriskie/axios/blob/master/COOKBOOK.md#jsonp
+    userMessage (successCallback, errorCallback) {
+        jsonp(baseUrl + 'user/info/jsonp_get_current', {name:'callback'}, function (error, response) {
+            if (error) {
+                errorCallback && errorCallback(error);
+            } else {
+                successCallback && successCallback(response);
+            }
+        });
+    },
+    // 判断用户是否登录，登录返回true，反之返回false
+    // 调用loginUser.isLogin().then((value) => {console.log(value);});
+    isLogin () {
+        return new Promise(function(resolve, reject) {
+            jsonp(baseUrl + 'user/info/jsonp_get_current', {name:'callback'}, function (error, response) {
+                if (error) {
+                    reject(error);
+                } else {
+                    // smzdm_id为0表示未登录
+                    if (typeof response.smzdm_id ==='undefined' || response.smzdm_id === 0) {
+                        resolve(false);
+                    }else{
+                        resolve(true);
+                    }
+                }
+            });
+        })
+    },
     // 判断是ios还是安卓
     iosOrAdr () {
         var userAgent = navigator.userAgent;
@@ -11,4 +45,30 @@
             return 'ios';
         }
         return 'ios';
+    },
+    // 调起原生登录
+    nativeLogin(){
+        var iosOrAdrStr = this.iosOrAdr();
+        if (iosOrAdrStr === 'android') {
+            if (typeof call_client_login === 'function') {
+                call_client_login();
+            } else if (typeof window.call_client_login === 'function') {
+                window.call_client_login();
+            } else if (typeof window.smzdmwebaction.call_client_login === 'function') {
+                window.smzdmwebaction.call_client_login();
+            }
+        } else if (iosOrAdrStr === 'ios') {
+            if (getClient.isWkWebview) {
+                window.webkit.messageHandlers.call_client_login.postMessage(null);
+            } else {
+                if (typeof call_client_login === 'function') {
+                    call_client_login();
+                }
+            }
+            // if (typeof call_client_login === 'function') {
+            //     call_client_login();
+            // }
+        }
     }
+};
+/* eslint-enable */
